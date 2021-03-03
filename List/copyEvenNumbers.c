4e067d8dd40c6d83e7725c3659a7a020c9bfff43
Quang Thuy <qthuy2609@gmail.com>
2021-03-03 08:10:40 +0700

    Update

--- List/copyEvenNumbers.c
+++ List/copyEvenNumbers.c
@@ -1,11 +1,5 @@
 #include "AListLib.c"
 
-void insertSet(int x, List *pL){
-   int Last = pL->Last;
-   pL->Elements[Last] = x;
-   pL->Last++;
-}
-
 void copyEvenNumbers(List L1, List *pL2){
     makeNullList(pL2);
     Position P = firstList(L1);
@@ -19,17 +13,17 @@ void copyEvenNumbers(List L1, List *pL2){
 
 int main(){
     List L1,L2;
-int i;
-makeNullList(&L1);
-for(i=0;i<=5;i++){
-    insertList(i, L1.Last+1, &L1);
-}
-copyEvenNumbers(L1, &L2);
-for(i=0;i<L1.Last;i++){
-    printf("%d ",L1.Elements[i]);
-}
-printf("\n");
-for(i=0;i<L2.Last;i++){
-    printf("%d ",L2.Elements[i]);
-}
+    int i;
+    makeNullList(&L1);
+    for(i=0;i<=5;i++){
+        insertList(i, L1.Last+1, &L1);
+    }
+    copyEvenNumbers(L1, &L2);
+    for(i=0;i<L1.Last;i++){
+        printf("%d ",L1.Elements[i]);
+    }
+    printf("\n");
+    for(i=0;i<L2.Last;i++){
+        printf("%d ",L2.Elements[i]);
+    }
 }
